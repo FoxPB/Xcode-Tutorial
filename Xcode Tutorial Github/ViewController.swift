6eedb5d39f5eceb9a8d8a76faac3149ed2ea86fe
Ricardo Caldeira de Andrade <ricardo.caldeira3@hotmail.com>
2016-07-20 10:18:29 -0300

    Initial Commit

---
+++ Xcode Tutorial Github/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Xcode Tutorial Github
+//
+//  Created by Ricardo Caldeira on 20/07/16.
+//  Copyright © 2016 Ricardo Caldeira. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
